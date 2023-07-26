b9f80610d15cebecacab24c5c9be1137e4b69ad0
Rohit Kumar Pal <rkp7133@gmail.com>
2023-07-26 15:48:09 +0530

    push code

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import "./App.css";
+import React, { useState } from "react";
+import { Alert } from "./component/Alert";
+import { About } from "./component/About";
+import { Navbar } from "./component/Navbar.jsx";
+import { TextForm } from "./component/TextForm.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+    }
+
+  };
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route path="about" element={<About mode={mode} />} />
+          <Route
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyse below"
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyse below"
+                mode={mode}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router> */}
+    </>
+  );
+}
+
+export default App;
